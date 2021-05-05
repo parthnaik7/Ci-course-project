725426c3108836f9356760bd3b677d65e8147a90
Parth Naik <36201848+parthnaik7@users.noreply.github.com>
2021-05-05 20:32:51 +0530

    Update InstallChrome.sh

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -2,3 +2,4 @@
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo chmod -x " /usr/bin/google-chrome"
